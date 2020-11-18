b7c32d886231330498f0857cc47c5aa65204cf0b
Ingo Breßler <dev@ingobressler.net>
2020-11-18 11:52:33 +0100

    appveyor_keep_builds: assert that history was retrieved successfully

--- appveyor_keep_builds.py
+++ appveyor_keep_builds.py
@@ -13,7 +13,7 @@ import argparse
 from requests import get, delete # for HTTP requests
 from dateutil.parser import parse as dateutil_parse
 from operator import itemgetter
-from helpers import makeRequest, assertEnvVarExists
+from helpers import makeRequest, assertEnvVarExists, jsonPrettyPrint
 
 parser = argparse.ArgumentParser(description='Remove old AppVeyor builds.')
 parser.add_argument('buildsToKeep', metavar='N', type=int, default=20,
@@ -35,6 +35,11 @@ authHead = {"Authorization": "Bearer " + os.environ['APPVEYOR_TOKEN']}
 response, code = makeRequest(get, baseurl, f"/projects/{accountName}/{projectSlug}/history",
                              headers=authHead, verbose=args.verbose,
                              params=dict(recordsNumber=100))
+if code != 200:
+    print("Could not get build history! Stopping.")
+    sys.exit(1)
+
+jsonPrettyPrint(response) # for debugging
 builds = [(build['buildId'], dateutil_parse(build['finished'])) for build in response['builds']]
 builds.sort(key=itemgetter(-1))
 print(f"Found {len(builds)} builds.")
